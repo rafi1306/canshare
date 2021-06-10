d1cf26c2ff2481fce6cd531b76106159a6ec4aee
Eryana Nurwenda <78951884+eryananurwenda@users.noreply.github.com>
2021-06-11 01:30:59 +0700

    update login

---
+++ login_reset_cek_email.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="css/tailwind.css">
+    <title>Login</title>
+</head>
+<body>
+    <div class="grid justify-items-center w-full min-h-screen">
+        <div class="grid self-center">
+            <div class="bg-red-100 shadow-lg overflow-hidden max-w-lg rounded-2xl self-center p-10">
+                <form action="" method="post">
+                <div class="grid grid-cols-1 gap-4">
+                <center><img src="assets/37ee585734f72e6bd2d3102f6994813b.png" width="75px"></center>
+                    <b class="text-center mt-3 font-bold text-2xl text-red-500">Yuk cek emailmu untuk ganti kata sandi</b>
+                    <div class="bg-red-200" style="height: 2px;"></div>
+                    <p class="text-center">Belum punya akun? <a href="" class="underline text-blue-500 text-center">Daftar Disini</a></p>
+                </div>
+                </form>
+            </div>
+            <div class="flex flex-row justify-center gap-6 mt-14 text-gray-500">
+                <a href="">About</a>
+                <a href="">Terms and Condition</a>
+                <a href="">Canshare 2020</a>
+            </div>
+        </div>
+    </div>
+</body>
+</html>
